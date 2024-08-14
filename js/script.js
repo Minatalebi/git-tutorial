3e55e00036fcccf1ec79c275d48194edfec66696
Mina Talebi <mina.talebi726@gmail.com>
2024-08-14 21:09:46 +0430

    add file

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const data = [
+  "+989393676765",
+  "+989393456123",
+  "+989393455465",
+  "+98393456722",
+  "+989393456765",
+];
+
+const length = data.length;
+const middleIndex = Math.floor(length / 2);
+const middleItem = data[middleIndex];
+const converterNumber = middleItem.replace("+98", "0");
+console.log(converterNumber);
+
+const email = "milad.azami@gmail.com";
+const result = email.split("@");
+console.log(result[0]);
